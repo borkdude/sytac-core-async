5e7b0102507c76b67f84115f877d119b53893cc0
Michiel Borkent <michielborkent@gmail.com>
2015-07-29 18:33:57 +0200

    Latest changes

--- code/cljs-demo/src/cljs_demo/main.cljs
+++ code/cljs-demo/src/cljs_demo/main.cljs
@@ -1,10 +1,12 @@
 (ns cljs-demo.main
   (:require-macros [cljs.core.async.macros :refer (go)])
   (:require
-   [cljs.core.async :refer (<!)]))
+   [cljs.core.async :refer (<!)]
+   [goog.dom :as dom]))
 
 (enable-console-print!)
 
+;; defining functions
 ;; (defn f [x]
 ;;     (inc x))
 
@@ -15,6 +17,7 @@
 ;; (defn f [x]
 ;;   (set! x "bar"))
 
+;; ;; basic js interop
 ;; (def foo (js-obj "bar" "baz"))
 ;; (set! (.-bar foo) "baaz")
 ;; (aset foo "abc" 17)
@@ -22,6 +25,47 @@
 ;; (js/alert "foo")
 ;; (.getTime (js/Date.))
 ;; (.. (js/Date.) (getTime) (toString))
+
+;; ;; dom manipulation
+
+;; (defn clear! []
+;;   (dom/removeChildren
+;;    (.-body js/document)))
+
+;; (clear!)
+
+;; (defn add-elt [e]
+;;   (dom/appendChild
+;;    (.-body js/document) e))
+
+;; (let [h1 (dom/createDom "h1" nil "Hello world")]
+;;   (add-elt h1))
+
+;; (let [ul (dom/createDom "ul" nil)]
+;;   (doseq [i (range 10)]
+;;     (dom/appendChild ul
+;;                      (dom/createDom "li" nil (str i))))
+;;   (add-elt ul))
+
+;; (add-elt (dom/createDom "pre"
+;;                         #js {:style "font-size: 5em;"}
+;;                         (pr-str (+ 1 2 3))))
+
+;; (defn show! [expr]
+;;   (clear!)
+;;   (let [body-style (.. js/document -body -style)]
+;;     (set! (.-backgroundColor body-style) "black")
+;;     (set! (.-color body-style) "green"))
+;;   (add-elt
+;;    (dom/createDom "div" #js {:style "margin: 50px;"}
+;;                   (dom/createDom "pre"
+;;                                  #js {:style (str
+;;                                               "font-size: 3em;"
+;;                                               "white-space: pre-wrap;")}
+;;                                  (pr-str expr)))))
+
+;; (show! (reductions + (range 10)))
+;; (show! (js/Date.))
 
 ;; ;; vectors
 ;; (def v [1 2 3])
@@ -82,3 +126,4 @@
 ;;   '(->> (range 10)
 ;;         (filter odd?)
 ;;         (map inc)))
+
